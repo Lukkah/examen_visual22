f609e15e3cc493ec6ab89300b8b5c06506651b5b
Deluk <Deluk@G480-Note>
2022-02-11 12:07:53 -0300

    Issue: Subida del Proyecto Responsable: Lucas, Luis Fernando Descripción: Subir el proyecto inicial a github Close #1

---
+++ Lucas_Luis_3605_Examen_Regular/src/main/java/ar/edu/unju/fi/examen/LucasLuis3605ExamenRegularApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasLuis3605ExamenRegularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasLuis3605ExamenRegularApplication.class, args);
+	}
+
+}
---
+++ Lucas_Luis_3605_Examen_Regular/src/test/java/ar/edu/unju/fi/examen/LucasLuis3605ExamenRegularApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasLuis3605ExamenRegularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
